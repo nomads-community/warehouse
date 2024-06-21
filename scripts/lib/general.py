3715b5fbad42383e19fecec5fda33e463d8498c8
Dan Bridges <danieljbridges@gmail.com>
2024-06-21 08:11:33 +0200

    Modified template regex to include .xlsm files

--- scripts/lib/general.py
+++ scripts/lib/general.py
@@ -7,7 +7,7 @@ class Regex_patterns():
     SEQDATA_BAMSTATS_CSV=re.compile(r'.*bam_flagstats.*.csv')    
     SEQDATA_BEDCOV_CSV=re.compile(r'.*summary.*bedcov.*.csv')
     NOMADS_EXPID=re.compile(r"(SW|PC|SL)[a-zA-Z]{2}[0-9]{3}")
-    NOMADS_EXP_TEMPLATE=re.compile(r".*(SW|PC|SL)[a-zA-Z]{2}[0-9]{3}.*.xlsx")
+    NOMADS_EXP_TEMPLATE=re.compile(r".*(SW|PC|SL)[a-zA-Z]{2}[0-9]{3}.*.xls(x|m)")
 
     #Files that are open
     EXCEL_FILES = re.compile(r"^[/.|~]")
